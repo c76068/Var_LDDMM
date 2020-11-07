1bb1248b4d3c9afef9f081810384f059fdee24e1
charoncode <74079903+charoncode@users.noreply.github.com>
2020-11-06 19:27:54 -0500

    Add files via upload

---
+++ src/libkeops-master/pykeops/generate_wheel.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#this script creates the wheel package for pykeops. Use at your own risk.
+VERSION='0.0.11'
+
+CURRENT_DIR=$(pwd)
+echo $CURRENT_DIR
+
+# ensure we are in the right dir
+SCRIPT_DIR=`dirname $0`
+echo $SCRIPT_DIR
+
+cd $SCRIPT_DIR
+cd ..
+
+# clean pycache stuff
+find -name *__pycache__* -exec rm {} \-rf \;
+find -name *.pyc* -exec rm {} \-rf \;
+
+# ugly trick to set right relative path in wheel package 
+cp readme.md pykeops/readme.md
+mv pykeops/__init__.py pykeops/__init__.py.src
+mv pykeops/__init__.py.wheel pykeops/__init__.py
+sed -i.bak "s/???/$VERSION/" pykeops/__init__.py
+sed -i.bak "s/???/$VERSION/" setup.py
+
+cp -R keops pykeops/
+
+# generate wheel
+python3 setup.py sdist --dist-dir build/dist
+python3 setup.py bdist_wheel --python-tag py3 --dist-dir build/wheel #--plat-name manylinux1_x86_64
+
+# undo ugly trick
+rm pykeops/readme.md
+mv pykeops/__init__.py.bak pykeops/__init__.py.wheel
+mv pykeops/__init__.py.src pykeops/__init__.py
+rm -rf pykeops/keops
+mv setup.py.bak setup.py
+
+# comes back to directory of 
+cd $CURRENT_DIR
---
+++ src/libkeops-master/pykeops/pykeops.md
@@ -0,0 +1,17 @@
+```
+ _____                 _  __     ____
+ \__  )               | |/ /    / __ \                 /\
+   / /     _ __  _   _| ' / ___| |  | |_ __  ___      /  \
+  / /     | '_ \| | | |  < / _ \ |  | | '_ \/ __|    / /\ \
+ | |__    | |_) | |_| | . \  __/ |__| | |_) \__ \   / /__\ \
+  \__ \   | .__/ \__, |_|\_\___|\____/| .__/|___/  /________\
+     ) )  | |     __/ |               | |
+    (_/   |_|    |___/                |_|
+```
+PyKeOps contains the python bindings (numpy and pytorch) for the cpp/cuda library [KeOps](https://plmlab.math.cnrs.fr/benjamin.charlier/libkeops/wikis/home). It provides
+standard python functions that can be used in any **python 3** codes.
+
+For a full documentation you may read the wiki:
+
+- [Installation](https://plmlab.math.cnrs.fr/benjamin.charlier/libkeops/wikis/python/Installation)
+- [Documentation](https://plmlab.math.cnrs.fr/benjamin.charlier/libkeops/wikis/python/Documentation)
